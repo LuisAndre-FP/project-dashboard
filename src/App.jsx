5dfb525874983647ac77644bf6e5717f3ddf059e
Luis Pacheco <luisandrefranco32@gmail.com>
2025-08-22 16:40:43 -0300

    login +ou-

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,16 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './styles/global.scss'
-import Login from './pages/Login'
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./styles/global.scss";
+import Login from "./pages/Login";
 
 function App() {
- 
-
   return (
     <>
-      <h1>Conecte se</h1>
       <div className="card">
-        <Login/>
+        <Login />
       </div>
-     
-
-    
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,20 +1,44 @@
-import { useState } from 'react'
-
-function Login () {
-      const [login, setLogin] = useState(null)
-      const [senha, setSenha] = useState(null)
-      return (
-        <>
+import { useState } from "react";
+import "../styles/global.scss";
+function Login() {
+  const [login, setLogin] = useState(null);
+  const [senha, setSenha] = useState(null);
+  return (
+    <>
+      <div className="rigth-side">
+        <h2>Sign in</h2>
         <form>
-            <input type='text' name='login' placeholder='Login' value={login} onChange={(e) => {
-                setLogin(e.target.value)
-            }}/>
-            <input type='password' name='senha' placeholder='Senha' value={senha} onChange={(e) => {
-                setSenha(e.target.value)
-            }} />
-            <button type='submit'>Login</button>
-        </form> 
-        </>
-      )
+          <input
+            type="text"
+            name="login"
+            placeholder="Enter email or user name"
+            value={login}
+            onChange={(e) => {
+              setLogin(e.target.value);
+            }}
+          />
+          <input
+            type="password"
+            name="senha"
+            placeholder="Password"
+            value={senha}
+            onChange={(e) => {
+              setSenha(e.target.value);
+            }}
+          />
+          <button type="submit">Login</button>
+        </form>
+
+        <div className="social-login">
+          <p>or continue with</p>
+          <div className="icons">
+            <img src="/Facebook.png" alt="Facebook" />
+            <img src="/apple.png" alt="Apple" />
+            <img src="/google.png" alt="Google" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
-export default Login;   
+export default Login;
